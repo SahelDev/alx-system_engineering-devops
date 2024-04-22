7519cfe2430fef1afa142617d013124435f26661
denzel84 <baderou@yahoo.fr>
2024-04-22 22:57:32 +0100

    task01

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-# Module that calls RESTful API
+
+
+"""gets api"""
 import requests
 from sys import argv
 
 
-if __name__ == "__main__":
-    '''Gives name of employee and completed tasks'''
+def fonction(userid):
+    """doc stringed"""
+    name = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(
+            userid)).json().get('name')
+    tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+            userid)).json()
+    tasksDone = ['\t {}\n'.format(dic.get('title')) for dic in tasks
+                 if dic.get('completed')]
+    if name and tasks:
+        print("Employee {} is done with tasks({}/{}):".format
+              (name, len(tasksDone), len(tasks)))
+        print(''.join(tasksDone), end='')
 
-    if len(argv) is not 2:
-        print("Command takes 2 arguments")
-        exit
-    _id = argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(_id)
-    req = requests.get(url)
-    jreq = req.json()
-    name = jreq['name']
-    url = "https://jsonplaceholder.typicode.com/todos"
-    req = requests.get(url)
-    jreq = req.json()
-    tasks = []
-    completed_tasks = []
-    for i in jreq:
-        if i['userId'] == int(_id):
-            tasks.append(i)
-    total_tasks = len(tasks)
-    for i in tasks:
-        if i['completed'] is True:
-            completed_tasks.append(i)
-    print("Employee {} is done with tasks({}/{}):".format(name,
-                                                          len(completed_tasks),
-                                                          total_tasks))
-    for i in completed_tasks:
-        print("\t {}".format(i['title']))
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        fonction(int(argv[1]))
---
+++ 0x15-api/0-gather_data_from_an_API_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# Module that calls RESTful API
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    '''Gives name of employee and completed tasks'''
+
+    if len(argv) is not 2:
+        print("Command takes 2 arguments")
+        exit
+    _id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(_id)
+    req = requests.get(url)
+    jreq = req.json()
+    name = jreq['name']
+    url = "https://jsonplaceholder.typicode.com/todos"
+    req = requests.get(url)
+    jreq = req.json()
+    tasks = []
+    completed_tasks = []
+    for i in jreq:
+        if i['userId'] == int(_id):
+            tasks.append(i)
+    total_tasks = len(tasks)
+    for i in tasks:
+        if i['completed'] is True:
+            completed_tasks.append(i)
+    print("Employee {} is done with tasks({}/{}):".format(name,
+                                                          len(completed_tasks),
+                                                          total_tasks))
+    for i in completed_tasks:
+        print("\t {}".format(i['title']))
